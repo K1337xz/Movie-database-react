d48a7d8f79bac8ee5617c2b8eb9ecf437b700596
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-08-02 16:28:48 +0200

    update

---
+++ client/src/components/dropdownSortMenu/DropdownSortMenu.jsx
@@ -0,0 +1,78 @@
+import "./dropdownsortmenu.scss";
+
+export default function DropdownSortMenu(props) {
+	return (
+		<form className={props.classDrop} onChange={props.onChange}>
+			<input
+				type="radio"
+				name="option"
+				id="popularityDescending"
+				value="Popularity Descending"
+			/>
+			<label
+				htmlFor="popularityDescending"
+				className="dropDownSort__option"
+			>
+				Popularity Descending
+			</label>
+			<input
+				type="radio"
+				name="option"
+				id="popularityAscending"
+				value="Popularity Ascending"
+			/>
+			<label
+				htmlFor="popularityAscending"
+				className="dropDownSort__option"
+			>
+				Popularity Ascending
+			</label>
+			<input
+				type="radio"
+				name="option"
+				id="ratingAscending"
+				value="Rrating Ascending"
+			/>
+			<label htmlFor="ratingAscending" className="dropDownSort__option">
+				Rating Ascending
+			</label>
+			<input
+				type="radio"
+				name="option"
+				id="ratingDescending"
+				value="Rating Descending"
+			/>
+			<label htmlFor="ratingDescending" className="dropDownSort__option">
+				Rating Descending
+			</label>
+			<input
+				type="radio"
+				name="option"
+				id="relaseDateDescending"
+				value="Relase Date Descending"
+			/>
+			<label
+				htmlFor="relaseDateDescending"
+				className="dropDownSort__option"
+			>
+				Relase Date Descending
+			</label>
+			<input
+				type="radio"
+				name="option"
+				id="relaseDateAescending"
+				value="Relase Date Aescending"
+			/>
+			<label
+				htmlFor="relaseDateAescending"
+				className="dropDownSort__option"
+			>
+				Relase Date Aescending
+			</label>
+			<input type="radio" name="option" id="titleAZ" value="Title A-Z" />
+			<label htmlFor="titleAZ" className="dropDownSort__option">
+				Title (A-Z)
+			</label>
+		</form>
+	);
+}
--- client/src/pages/nowPlaying/NowPlaying.jsx
+++ client/src/pages/nowPlaying/NowPlaying.jsx
@@ -5,8 +5,16 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import "./nowplaying.scss";
 import Nav from "../../components/Navbar/Nav";
 import Footer from "../../components/Footer/Footer";
+import DropdownSortMenu from "../../components/dropdownSortMenu/dropdownSortMenu";
 
 export default function NowPlaying() {
+	const [toggleDropDown, setToggleDropDown] = useState(false);
+	const [sortType, setSortType] = useState("");
+	const toggleChange = (e) => {
+		setSortType(e.target.value);
+		setToggleDropDown(false);
+	};
+	console.log(sortType);
 	return (
 		<>
 			<Nav />
@@ -16,28 +24,30 @@ export default function NowPlaying() {
 						<h1>Now Playing</h1>
 						<div className="nowPlaying__buttonsWrapper nowPlaying__buttonsWrapper--open">
 							<div className="nowPlaying__sortOptions">
-								<select className="nowPlaying__selectSort">
-									<option value="select">Sort By</option>
-									<option value="popularityDescending">
-										Popularity Descending
-									</option>
-									<option value="popularityAscending">
-										Popularity Ascending
-									</option>
-									<option value="ratingAscending">
-										Rating Ascending
-									</option>
-									<option value="ratingAscending">
-										Rating Ascending
-									</option>
-									<option value="relaseDateDescending">
-										Relase Date Ascending
-									</option>
-									<option value="relaseDateDescending">
-										Relase Date Ascending
-									</option>
-									<option value="titleAZ">Title (A-Z)</option>
-								</select>
+								<span
+									className={
+										toggleDropDown
+											? "nowPlaying__sortToggle--open"
+											: "nowPlaying__sortToggle"
+									}
+									onClick={() => {
+										setToggleDropDown(
+											(prev) => (prev = !prev)
+										);
+									}}
+								>
+									{sortType.length > 0 ? sortType : "Sort By"}
+								</span>
+								{toggleDropDown ? (
+									<DropdownSortMenu
+										classDrop={"dropDownSort--open"}
+										onChange={toggleChange}
+									/>
+								) : (
+									<DropdownSortMenu
+										classDrop={"dropDownSort"}
+									/>
+								)}
 							</div>
 						</div>
 					</div>
