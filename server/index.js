bfc2f428840515bd249ca711a2d898ac811449b0
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-09-20 17:32:22 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,10 +25,7 @@ app.use((req, res, next) => {
 });
 app.use(
 	cors({
-		origin: [
-			"https://movie-database-react-five.vercel.app",
-			"https://moviedb-q5pg.onrender.com/",
-		],
+		origin: ["https://movie-database-react-five.vercel.app"],
 		credentials: true,
 	})
 );
