2025e9bb55045fd8cce8964d69740fab965c38f2
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-09-26 16:34:57 +0200

    Update Userpage.jsx

--- client/src/pages/userpage/Userpage.jsx
+++ client/src/pages/userpage/Userpage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import Nav from "../../components/Navbar/Nav";
 import Footer from "../../components/Footer/Footer";
@@ -6,13 +7,20 @@ import noavatar from "../../assets/pngwing.com.png";
 import "./Userpage.scss";
 
 export default function Userpage() {
+	const navigate = useNavigate();
 	const { currentUser } = useContext(AuthContext);
+
 	return (
 		<>
 			<Nav />
 			<main className="container">
 				<div className="profile">
-					<h1>WELCOME {currentUser.username}!!!!</h1>
+					<div className="profile__topContent">
+						<div className="profile__avatarSection">
+							<img src={currentUser.img || noavatar} />
+							<p>{currentUser.username}</p>
+						</div>
+					</div>
 				</div>
 			</main>
 			<Footer />
