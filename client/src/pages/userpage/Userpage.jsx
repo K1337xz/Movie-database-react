af865b08e549080cdd67e5d8beb5ab0dcc267e19
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-09-29 18:25:14 +0200

    Update userpage

--- client/src/pages/userpage/Userpage.jsx
+++ client/src/pages/userpage/Userpage.jsx
@@ -19,9 +19,19 @@ export default function Userpage() {
 		},
 	];
 	const [active, setActive] = useState("About Me");
+	const [descriptionInputValue, setDescriptionInputValue] = useState({});
+	const [description, setDescription] = useState(false);
 	const navigate = useNavigate();
 	const { currentUser } = useContext(AuthContext);
 
+	const toggleAddDescription = () => {
+		setDescription(true);
+	};
+	const handleChange = (e) => {
+		setDescriptionInputValue({
+			[e.target.name]: e.target.value,
+		});
+	};
 	return (
 		<>
 			<Nav />
@@ -57,7 +67,52 @@ export default function Userpage() {
 							</ul>
 						</div>
 					</div>
-					<div className="profile__content"></div>
+					<div className="profile__content">
+						{active === "About Me" && (
+							<div className="profile__aboutMe">
+								{currentUser.desc ? (
+									currentUser.desc
+								) : description ? (
+									<form className="profile__form">
+										<label htmlFor="description">
+											<input
+												id="description"
+												name="description"
+												type="text"
+												className="profile__form--input"
+												onChange={handleChange}
+											/>
+										</label>
+										<div className="profile__form-buttons">
+											<button className="profile__form--cancelBtn">
+												Cancel
+											</button>
+											<button className="profile__form--submitBtn">
+												Add Description
+											</button>
+										</div>
+									</form>
+								) : (
+									<p
+										className="profile__aboutMe--addDescription"
+										onClick={toggleAddDescription}
+									>
+										Click to add your description
+									</p>
+								)}
+							</div>
+						)}
+						{active === "Reviews" && (
+							<div>
+								<h2>Reviews</h2>
+							</div>
+						)}
+						{active === "Settings" && (
+							<div>
+								<h2>Settings</h2>
+							</div>
+						)}
+					</div>
 				</div>
 			</main>
 			<Footer />
